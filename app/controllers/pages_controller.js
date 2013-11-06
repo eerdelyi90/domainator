781ad5b93c1f6709c36e96d57e84221e807bb3a5
Egon Erdelyi <eerdelyi90@gmail.com>
2013-11-06 12:26:50 +0000

    added the controller to login

--- app/controllers/pages_controller.js
+++ app/controllers/pages_controller.js
@@ -1,5 +1,6 @@
 var locomotive = require('locomotive')
-  , Controller = locomotive.Controller;
+  , Controller = locomotive.Controller
+  , User       = require('../models').User;
 
 var PagesController = new Controller();
 
@@ -8,9 +9,20 @@ PagesController.main = function() {
   this.stuff ='stuff';
   this.render();
 }
-PagesController.login = function() {
+PagesController.login = function(req, res) {
   this.title = 'login';
   this.stuff ='login';
+
+  console.log(req);
+
+  User.findAll({ where: ["username = ?" , req.body.username],["password = ?", req.body.password}).success(function(users) {
+    if(users.length > 0)
+      // Greate
+      console.log("GREAAAT");
+    else
+      // NOOs
+      console.log("NOOO");
+  });
   this.render();
 }
 PagesController.settings = function() {
